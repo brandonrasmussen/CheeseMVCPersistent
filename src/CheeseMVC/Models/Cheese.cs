9b0c9d5d10fccf12cd78ba350daba798898693b6
brandonrasmussen2 <brandonrasmussen2@gmail.com>
2018-03-04 16:05:17 -0800

    Code before resetting entity framework migrations to a clean slate

--- src/CheeseMVC/Models/Cheese.cs
+++ src/CheeseMVC/Models/Cheese.cs
@@ -1,10 +1,16 @@
-﻿namespace CheeseMVC.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.Models
 {
     public class Cheese
     {
         public string Name { get; set; }
         public string Description { get; set; }
-        public CheeseType Type { get; set; }
+        public CheeseCategory Category { get; set; }
+        public int CategoryID { get; set; }
         public int ID { get; set; }
     }
 }
